78bc1c89be200a6b879693a2a0c840177d5dc886
melashu <meshu102@outlook.com>
2022-10-15 00:55:06 +0300

    Add routing configration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import Header from './coponents/header/Header';
+import Detail from './pages/Detail/Detail';
+import Home from './pages/Home/Home';
+import './App.scss';
+import Footer from './coponents/footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="cryptodetail/:id" element={<Detail />} />
+        </Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
