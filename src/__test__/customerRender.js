ccd92469dce4609ba0224972f6bf9464592e7b95
melashu <meshu102@outlook.com>
2022-10-15 00:56:03 +0300

    Create custom render method for integration testing

---
+++ src/__test__/customerRender.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { BrowserRouter } from 'react-router-dom';
+import cryptoreducer from '../redux/cryptoreducer';
+
+export default function customerRender(
+  component,
+  {
+    store = configureStore({
+      reducer: { crypto: cryptoreducer },
+    }),
+    ...renderOptions
+  } = {},
+) {
+  function Wrapper(prop) {
+    const { children } = prop;
+    return (
+      <BrowserRouter>
+        <Provider store={store}>{children}</Provider>
+      </BrowserRouter>
+    );
+  }
+  return {
+    store,
+    ...render(component, { wrapper: Wrapper, ...renderOptions }),
+  };
+}
