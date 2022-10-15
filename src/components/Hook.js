79a000041a3b9a449ab4a2d57681196e463d7669
melashu <meshu102@outlook.com>
2022-10-15 10:30:07 +0300

    Chnage file extension from jsx to js

---
+++ src/components/Hook.js
@@ -0,0 +1,5 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+const useCryptoSelector = useSelector;
+const useCryptoDispatch = useDispatch;
+export { useCryptoDispatch, useCryptoSelector };
---
+++ src/components/header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+import SettingsIcon from '@mui/icons-material/Settings';
+import './header.scss';
+
+const Header = () => {
+  const currentLocation = useLocation();
+  return (
+    <div className="header">
+      <div className="logo-container">
+        {currentLocation.pathname !== '/' && (
+          <Link to="/" data-testid="backbutton">
+            <span>
+              <ChevronLeftIcon className="icon" />
+            </span>
+          </Link>
+        )}
+        <Link to="/" className="logo">
+          {' '}
+          <div className="logo">Live Crypto</div>
+        </Link>
+      </div>
+      <div className="icon-conatiner">
+        <KeyboardVoiceIcon className="icon" data-testid="back-icon" />
+        <SettingsIcon className="icon" data-testid="setting" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
