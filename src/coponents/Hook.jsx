f800fce36f0cca53fde7391aabcf17669a122794
melashu <meshu102@outlook.com>
2022-10-15 10:34:30 +0300

    Change file extension from jsx to js

--- src/coponents/Hook.jsx
+++
@@ -1,5 +0,0 @@
-import { useSelector, useDispatch } from 'react-redux';
-
-const useCryptoSelector = useSelector;
-const useCryptoDispatch = useDispatch;
-export { useCryptoDispatch, useCryptoSelector };
